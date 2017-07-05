d1c48151b19cc47241dfb419d2aa104c33006031
javad <javad@javads-MacBook-Air.local>
2017-07-05 17:01:00 +0430

    Creat view pager listener & base resourse for background

--- app/src/main/java/com/example/android/udacity/MainActivity.java
+++ app/src/main/java/com/example/android/udacity/MainActivity.java
@@ -15,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
         CustomPagerAdapter adapter = new CustomPagerAdapter(this);
 
         viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                switch (position){
+
+                    case 0:
+
+                        break;
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
